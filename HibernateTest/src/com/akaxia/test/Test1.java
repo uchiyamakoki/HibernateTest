7375072e5b7a5cd06b6fb9da237c9d2dc584134b
momoka <454768497@qq.com>
2018-01-19 00:10:38 +0800

    稍作修改

--- HibernateTest/src/com/akaxia/test/Test1.java
+++ HibernateTest/src/com/akaxia/test/Test1.java
@@ -1,9 +1,14 @@
 package com.akaxia.test;
 
 import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Transaction;
 
 import com.akaxia.dao.SjzdflDAO;
 import com.akaxia.po.Sjzdfl;
+import com.akaxia.po.Sjzdxx;
+import com.akaxia.sessionFactory.HibernateSessionFactory;
 
 public class Test1 {
 
@@ -11,20 +16,43 @@ public class Test1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SjzdflDAO sdao=new SjzdflDAO();
-		/*
-		 * 
-		 * 
-		List<Sjzdfl> all=sdao.findAll();
-		for(Sjzdfl sjzdfl:all){
+		SjzdflDAO sjzdflDAO=new SjzdflDAO();
+		//第一种方法
+		//Sjzdfl sjzdfl=new Sjzdfl();
+		//sjzdfl.setSjzdflid(6);
+		Sjzdfl sjzdfl=sjzdflDAO.findById(6);
+		//sjzdfl.setSjzdflmc("测试更新数据字典分类");
+		
+		
+		Transaction transaction=HibernateSessionFactory.getSession().beginTransaction();
+		
+		//sjzdflDAO.attachDirty(sjzdfl);
+		sjzdflDAO.delete(sjzdfl);
+		//sjzdflDAO.save(sjzdfl);
+		
+		transaction.commit();
+		
+		
+		//Sjzdfl sjzdfl=sjzdflDAO.findById(1);
+		//Sjzdfl sjzdfl=(Sjzdfl) sjzdflDAO.findBySjzdflmc("企业类型-分类").get(0);
+		//Sjzdfl sjzdfl=(Sjzdfl) sjzdflDAO.findByProperty("sjzdflid", 3).get(0);
+		//System.out.println(sjzdfl.getSjzdflid());
+		//System.out.println(sjzdfl.getSjzdflmc());
+		
+		//findall测试
+		/*List<Sjzdfl> sjzdflAll=sjzdflDAO.findAll();
+		
+		for(Sjzdfl sjzdfl:sjzdflAll){
 			System.out.println(sjzdfl.getSjzdflid());
 			System.out.println(sjzdfl.getSjzdflmc());
-		}
+			Set<Sjzdxx> sjzdxxAll= sjzdfl.getSjzdxxes();
+			for(Sjzdxx sjzdxx:sjzdxxAll){
+				System.out.println(sjzdxx.getSjzdxxmc()+",");
+			}
+			System.out.println();
+			System.out.println("---------------------------");
+		}*
 		*/
-		Sjzdfl sjzdfl=sdao.findById(1);
-		System.out.println(sjzdfl.getSjzdflid());
-		System.out.println(sjzdfl.getSjzdflmc());
 	}
 
 }
